7088329be2d3357774a34a1a4b9c6390f92409bf
John Doe <lookatitonce@gmail.com>
2023-01-30 09:39:34 +0530

    missing html part in the render fuction

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-// Get the form, input, and list elements
-const todoForm = document.querySelector("#todo-form");
-const todoInput = document.querySelector("#todo-input");
-const todoList = document.querySelector("#todo-list");
+// Todo List App
 
-// Get the todo items from local storage, or an empty array if none exist
+// Declare variables
+const todoForm = document.querySelector(".todo-form");
+const todoInput = document.querySelector(".todo-input");
+const todoList = document.querySelector(".todo-list");
+
 let todoItems = JSON.parse(localStorage.getItem("todoItems")) || [];
 
-// Add a new todo item to the list
+// Add a new todo item
 function addTodo(text) {
   const todo = {
     text,
@@ -17,16 +18,28 @@ function addTodo(text) {
   renderTodoList();
 }
 
+// Remove a todo item
+function removeTodo(event) {
+  const button = event.target;
+  const li = button.parentElement;
+  const index = Array.from(todoList.children).indexOf(li);
+  todoItems.splice(index, 1);
+  localStorage.setItem("todoItems", JSON.stringify(todoItems));
+  renderTodoList();
+}
+
 // Render the todo list
 function renderTodoList() {
   todoList.innerHTML = "";
-  for (let i = 0; i < todoItems.length; i++) {
-    const todo = todoItems[i];
+  for (let todo of todoItems) {
     const li = document.createElement("li");
-    li.innerText = todo.text;
+    li.innerHTML = `
+      ${todo.text} 
+      <button class="remove-todo">X</button>
+    `;
     li.classList.add("todo-item");
     if (todo.completed) {
-    li.classList.add("completed");
+      li.classList.add("completed");
     }
     li.addEventListener("click", toggleCompleted);
     todoList.appendChild(li);
@@ -52,3 +65,61 @@ todoForm.addEventListener("submit", event => {
 
 // Render the initial todo list
 renderTodoList();
+
+// // Get the form, input, and list elements
+// const todoForm = document.querySelector("#todo-form");
+// const todoInput = document.querySelector("#todo-input");
+// const todoList = document.querySelector("#todo-list");
+
+// // Get the todo items from local storage, or an empty array if none exist
+// let todoItems = JSON.parse(localStorage.getItem("todoItems")) || [];
+
+// // Add a new todo item to the list
+// function addTodo(text) {
+//   const todo = {
+//     text,
+//     completed: false
+//   };
+
+//   todoItems.push(todo);
+//   localStorage.setItem("todoItems", JSON.stringify(todoItems));
+//   renderTodoList();
+// }
+
+// // Render the todo list
+// function renderTodoList() {
+//   todoList.innerHTML = "";
+//   for (let i = 0; i < todoItems.length; i++) {
+//     const todo = todoItems[i];
+//     const li = document.createElement("li");
+//     li.innerText = todo.text;
+//     li.classList.add("todo-item");
+    
+//     if (todo.completed) {
+//       li.classList.add("completed");
+//     }
+    
+//     li.addEventListener("click", toggleCompleted);
+//     todoList.appendChild(li);
+//   }
+// }
+
+// // Toggle the completed status of a todo item
+// function toggleCompleted(event) {
+//   const li = event.target;
+//   const index = Array.from(todoList.children).indexOf(li);
+//   todoItems[index].completed = !todoItems[index].completed;
+//   localStorage.setItem("todoItems", JSON.stringify(todoItems));
+//   renderTodoList();
+// }
+
+// // Handle form submission
+// todoForm.addEventListener("submit", event => {
+//   event.preventDefault();
+//   const text = todoInput.value;
+//   todoInput.value = "";
+//   addTodo(text);
+// });
+
+// // Render the initial todo list
+// renderTodoList();
